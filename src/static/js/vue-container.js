51ea61d18a1d3b0570418d0eea8c46f15b83f83f
Patrick Chan <patrickchan1999@gmail.com>
2018-03-21 22:28:30 -0400

    Change implementation of inline editing, add datetimepicker to form

--- src/static/js/vue-container.js
+++ src/static/js/vue-container.js
@@ -12,9 +12,9 @@ var vue = new Vue({
     el: '#vue-container',
     data: {
         tasks: [
-            {title: 'Dishes', body: 'Do the dishes', time: '2hrs left', editing: true},
-            {title: 'Trash', body: 'Take out the trash', time: '2hrs left', editing: false},
-            {title: 'Lawn', body: 'Mow the lawn', time: '2hrs left', editing: false}
+            {title: 'Dishes', body: 'Do the dishes', time: '03/20/2018 19:03:00', editing: true},
+            {title: 'Trash', body: 'Take out the trash', time: '03/20/2018 19:03:00', editing: false},
+            {title: 'Lawn', body: 'Mow the lawn', time: '03/20/2018 19:03:00', editing: false}
         ]
     },
     mounted: function() {
@@ -44,6 +44,15 @@ var vue = new Vue({
             if (event.detail.dragType === 'move') {
                 this.tasks.splice(endIndex, 0, this.tasks.splice(startIndex, 1)[0]);
             }
+        },
+
+        showDatePicker: function(event){
+            var datepicker = new MaterialDatetimePicker().on('submit', (val) => {
+                var e = new Event('input', {bubbles: true});
+                Vue.set(event.target, 'value', val.format('MM/DD/YYYY HH:MM:SS'));
+                event.target.dispatchEvent(e);
+            });
+            datepicker.open();
         }
     }
 });
