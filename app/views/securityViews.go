689be6cef3866a0bd81f434715466b015e8a6096
Patrick Chan <patrickchan1999@gmail.com>
2018-04-08 12:58:48 -0400

    Laid the foundation for authentication

---
+++ app/views/securityViews.go
@@ -0,0 +1,108 @@
+package views
+
+import (
+	"net/http"
+
+	"github.com/PGonLib/PGo-Auth/pkg/security"
+	"github.com/pchan37/tasky/app/lib/templateManager"
+)
+
+func RegisterSecurityViews() {
+	http.HandleFunc("/register", RegisterHandler)
+	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/logout", LogoutHandler)
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		templateManager.RenderTemplate(w, "register.tmpl", nil)
+	case http.MethodPost:
+		userCredential := &security.Credential{
+			Username:             r.FormValue("username"),
+			Password:             r.FormValue("password"),
+			ConfirmationPassword: r.FormValue("confirmationPassword"),
+		}
+		validateRegisterCredentials(w, r, userCredential)
+		if success := security.Register(userCredential); success {
+			fullCredential, _ := security.GetUserCredential(userCredential.Username)
+			session, err := security.GetSession(w, r)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			session.Values["authenticated"] = true
+			session.Values["role"] = fullCredential.Role
+			session.Save(r, w)
+			redirectBack(w, r)
+		}
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		templateManager.RenderTemplate(w, "login.tmpl", nil)
+	case http.MethodPost:
+		userCredential := &security.Credential{
+			Username: r.FormValue("username"),
+			Password: r.FormValue("password"),
+		}
+		if success := security.Login(userCredential); success {
+			fullCredential, _ := security.GetUserCredential(userCredential.Username)
+			session, err := security.GetSession(w, r)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			session.Values["authenticated"] = true
+			session.Values["role"] = fullCredential.Role
+			session.Save(r, w)
+			redirectBack(w, r)
+		}
+		addFlashMessage(w, r, "Incorrect username or password")
+		templateManager.RenderTemplate(w, "login.tmpl", nil)
+	}
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := security.GetSession(w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	session.Options.MaxAge = -1
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	templateManager.RenderTemplate(w, "index.tmpl", nil)
+}
+
+func addFlashMessage(w http.ResponseWriter, r *http.Request, message string) {
+	flashSession, _ := store.Get(r, "flashSession")
+	flashSession.AddFlash(message)
+	flashSession.Save(r, w)
+}
+
+func redirectBack(w http.ResponseWriter, r *http.Request) {
+	session, _ := security.GetSession(w, r)
+	if redirectURL, ok := session.Values["redirect-url"].(string); ok && redirectURL != "" {
+		session.Values["redirect-url"] = ""
+		session.Save(r, w)
+		http.Redirect(w, r, redirectURL, http.StatusTemporaryRedirect)
+	}
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
+
+func validateRegisterCredential(w http.ResponseWriter, r *http.Request, c *security.Credential) {
+	if security.IsRegistered(c.Username) {
+		addFlashMessage(w, r, "Username already taken!")
+		templateManager.RenderTemplate(w, "register.tmpl", nil)
+	} else if c.Password != c.ConfirmationPassword {
+		addFlashMessage(w, r, "Password does not match confirmation password!")
+		templateManager.RenderTemplate(w, "register.tmpl", nil)
+	}
+}
